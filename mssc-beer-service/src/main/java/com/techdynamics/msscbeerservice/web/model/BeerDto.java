179c6682facb52422f6395fd99140d24f9738c3c
syadav <syadav@gurlaptop248.bravurasolutions.local>
2020-06-04 19:29:52 +0530

    adding models

---
+++ mssc-beer-service/src/main/java/com/techdynamics/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,48 @@
+package com.techdynamics.msscbeerservice.web.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class BeerDto implements Serializable {
+
+	private static final long serialVersionUID = -2258701996014378912L;
+
+	@Null
+	private UUID id;
+
+	@Null
+	private Integer version;
+
+	@Null
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+	private OffsetDateTime createdDate;
+
+	@Null
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+	private OffsetDateTime lastModifiedDate;
+
+	@NotBlank
+	private String beerName;
+
+	@NotNull
+	private BeerStyleEnum beerStyle;
+
+	@NotNull
+	private String upc;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@Positive
+	@NotNull
+	private BigDecimal price;
+
+	private Integer quantityOnHand;
+}
